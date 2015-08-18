2b62d2f725c83f6e3d52cb7a60bcf462c38d562f
feSLsx <LukasJezMusic@gmail.com>
2015-08-18 14:31:02 -0800

    Pinned Tabs + Watermark are hidden when a user presses F3.
    Added an if method to make the watermark plus pinned tabs hidden when a
    user presses F3

--- net/yawk/client/hooks/GuiIngameHook.java
+++ net/yawk/client/hooks/GuiIngameHook.java
@@ -37,21 +37,20 @@ public class GuiIngameHook extends GuiIngame{
 	//private Camera cam = new Camera();
 	
 	@Override
-	public void func_175180_a(float p_175180_1_){
-		
+	public void func_175180_a(float p_175180_1_){	
 		super.func_175180_a(p_175180_1_);
-		
-		if(!hideClientMod.isEnabled()){
-			Client.getClient().getFontRenderer().drawStringWithShadow("Yawk" + EnumChatFormatting.GREEN + " v2.5" + EnumChatFormatting.LIGHT_PURPLE + " ("+(Client.getClient().getSession().isPremium()? "Premium":"Beta")+")", 3, 2, 0xFFFFFFFF, true);
-		}
-		
-		if(Client.getClient().getMinecraft().currentScreen == null){
-			for(Window win : Client.getClient().gui.windows){
-				if(win.pinned){
-					win.renderWindow(0, 0, false);
-				}
+		if(!Client.getClient().getMinecraft().gameSettings.showDebugInfo) {
+			if(!hideClientMod.isEnabled()){
+				Client.getClient().getFontRenderer().drawStringWithShadow("Yawk" + EnumChatFormatting.GREEN + " v2.5" + EnumChatFormatting.LIGHT_PURPLE + " ("+(Client.getClient().getSession().isPremium()? "Premium":"Beta")+")", 3, 2, 0xFFFFFFFF, true);
 			}
+			if(Client.getClient().getMinecraft().currentScreen == null){
+				for(Window win : Client.getClient().gui.windows){
+					if(win.pinned){
+						win.renderWindow(0, 0, false);
+					}
+				}
 		}
+	}
 		
 		EventManager.call(eventGuiRender);
 		
